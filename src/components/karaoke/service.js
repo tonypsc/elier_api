0bd6f653199498ded88ac07c5bf71bf261d9e23e
tonypsc <tonyps@gmail.com>
2022-08-24 17:51:28 -0400

    Add more details in youtube endpoint

--- src/components/karaoke/service.js
+++ src/components/karaoke/service.js
@@ -9,18 +9,41 @@ const repository = new sharedRepository('karaoke', 'karaoke_id');
 const service = {
 	// Calls youtube API to get search results
 	async getYoutube(search) {
-		const youtubeUrl =
-			'https://youtube.googleapis.com/youtube/v3/search?type=video&maxResults=20&part=snippet&q=karaoke ';
+		const youtubeSearchUrl =
+			'https://youtube.googleapis.com/youtube/v3/search?type=video&maxResults=20&q=karaoke ';
 
 		https: try {
-			const res = await fetch(
-				`${youtubeUrl}${search}&key=${config.GOOGLE_API_KEY}`
+			const searchResponse = await fetch(
+				`${youtubeSearchUrl}${search}&key=${config.GOOGLE_API_KEY}`
 			);
-			const data = await res.json();
 
-			const result = data.items.map((res) => {
+			const searchData = await searchResponse.json();
+
+			const idList = searchData.items.map((item) => item.id.videoId).join(',');
+
+			const youtubeVideoUrl =
+				'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics,snippet';
+
+			const videoResponse = await fetch(
+				`${youtubeVideoUrl}&id=${idList}&key=${config.GOOGLE_API_KEY}`
+			);
+
+			const videoData = await videoResponse.json();
+
+			const result = videoData.items.map((res) => {
 				res.link = `https://www.youtube.com/watch?v=${res.id.videoId}`;
-				return res;
+				return {
+					id: res.id,
+					link: `https://www.youtube.com/watch?v=${res.id.videoId}`,
+					thumbnail: res.snippet.thumbnail,
+					published: res.snippet.publishedAt,
+					title: res.snippet.title,
+					language: res.snippet.defaultAudioLanguage,
+					duration: res.contentDetails.duration,
+					views: res.statistics.viewCount,
+					likes: res.statistics.likeCount,
+					comments: res.statistics.commentCount,
+				};
 			});
 
 			return result;
