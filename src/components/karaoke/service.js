ea4edd84335a4be0541e2f57681625127c390be4
tonypsc <tonyps@gmail.com>
2022-08-25 10:54:25 -0400

    Homogenize youtube and dailymotion responses

--- src/components/karaoke/service.js
+++ src/components/karaoke/service.js
@@ -33,40 +33,23 @@ const service = {
 			const videoData = await videoResponse.json();
 
 			const result = videoData.items.map((res) => {
-				res.link = `https://www.youtube.com/watch?v=${res.id.videoId}`;
 				return {
 					id: res.id,
 					link: `https://www.youtube.com/watch?v=${res.id}`,
-					thumbnail: res.snippet.thumbnail,
+					thumbnail: {
+						small: res.snippet.thumbnails.default.url,
+						medium: res.snippet.thumbnails.medium.url,
+						high: res.snippet.thumbnails.high.url,
+					},
 					published: res.snippet.publishedAt,
 					title: res.snippet.title,
 					language: res.snippet.defaultAudioLanguage,
-					duration: res.contentDetails.duration,
-					views: res.statistics.viewCount,
-					likes: res.statistics.likeCount,
-					comments: res.statistics.commentCount,
+					duration: this.youtubeDurationToSeconds(res.contentDetails.duration),
+					views: parseInt(res.statistics.viewCount),
+					likes: parseInt(res.statistics.likeCount),
+					comments: parseInt(res.statistics.commentCount),
+					origin: 'youtube',
 				};
-			});
-
-			return videoData;
-		} catch (err) {
-			console.log(err);
-			return false;
-		}
-	},
-
-	// Calls daily API to get search results
-	async getDailyMotion(search) {
-		const youtubeUrl =
-			'https://api.dailymotion.com/videos?shorter_than=10&fields=id,title,thumbnail_url&search=karaoke ';
-
-		https: try {
-			const res = await fetch(`${youtubeUrl}${search}`);
-			const data = await res.json();
-
-			const result = data.list.map((res) => {
-				res.link = `https://www.dailymotion.com/video/${res.id}`;
-				return res;
 			});
 
 			return result;
@@ -74,6 +57,62 @@ const service = {
 			console.log(err);
 			return false;
 		}
+	},
+
+	// Calls dailyMotion API to get search results
+	async getDailyMotion(search) {
+		const dailyMotioneUrl =
+			'https://api.dailymotion.com/videos?shorter_than=10&fields=id,title,thumbnail_120_url,thumbnail_360_url,thumbnail_480_url,language,duration,views_total,likes_total&search=karaoke ';
+
+		https: try {
+			const res = await fetch(
+				search ? `${dailyMotioneUrl}${search}` : dailyMotioneUrl
+			);
+			const data = await res.json();
+
+			const result = data.list.map((res) => {
+				return {
+					id: res.id,
+					link: `https://www.dailymotion.com/video${res.id}`,
+					thumbnail: {
+						small: res.thumbnail_120_url,
+						medium: res.thumbnail_360_url,
+						high: res.thumbnail_480_url,
+					},
+					published: undefined,
+					title: res.title,
+					language: res.language,
+					duration: res.duration,
+					views: res.views_total,
+					likes: res.likes_total,
+					comments: 0,
+					origin: 'dailymotion',
+				};
+			});
+
+			return result;
+		} catch (err) {
+			console.log(err);
+			return false;
+		}
+	},
+	youtubeDurationToSeconds(duration) {
+		const match = duration.match(
+			/P(\d+Y)?(\d+W)?(\d+D)?T(\d+H)?(\d+M)?(\d+S)?/
+		);
+
+		// An invalid case won't crash the app.
+		if (!match) {
+			console.error(`Invalid YouTube video duration: ${duration}`);
+			return 0;
+		}
+		const [years, weeks, days, hours, minutes, seconds] = match
+			.slice(1)
+			.map((_) => (_ ? parseInt(_.replace(/\D/, '')) : 0));
+		return (
+			(((years * 365 + weeks * 7 + days) * 24 + hours) * 60 + minutes) * 60 +
+			seconds
+		);
 	},
 };
 
