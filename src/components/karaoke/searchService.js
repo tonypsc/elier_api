e615d077d3dbd3f75e7bd58eed51ea5a3d7c1bff
tonypsc <tonyps@gmail.com>
2022-10-03 18:14:31 -0400

    Format search result object to match db

--- src/components/karaoke/searchService.js
+++ src/components/karaoke/searchService.js
@@ -37,21 +37,19 @@ const searchService = {
 
 			const result = videoData.items.map((res) => {
 				return {
-					id: res.id,
+					karaoke_song_id: res.id,
+					origin: 'youtube',
 					link: `https://www.youtube.com/watch?v=${res.id}`,
-					thumbnail: {
-						small: res.snippet.thumbnails.default.url,
-						medium: res.snippet.thumbnails.medium.url,
-						high: res.snippet.thumbnails.high.url,
-					},
-					published: res.snippet.publishedAt,
 					title: res.snippet.title,
-					language: res.snippet.defaultAudioLanguage,
+					language: res.snippet.defaultAudioLanguage || 'en',
 					duration: this.youtubeDurationToSeconds(res.contentDetails.duration),
 					views: parseInt(res.statistics.viewCount),
 					likes: parseInt(res.statistics.likeCount),
 					comments: parseInt(res.statistics.commentCount),
-					origin: 'youtube',
+					thumbnail_small: res.snippet.thumbnails.default.url,
+					thumbnail_medium: res.snippet.thumbnails.medium.url,
+					thumbnail_high: res.snippet.thumbnails.high.url,
+					//published: res.snippet.publishedAt,
 					similarity: stringSimilarity.compareTwoStrings(
 						res.snippet.title.toLowerCase(),
 						`karaoke ${search.toLowerCase()}`
@@ -78,21 +76,18 @@ const searchService = {
 
 			const result = data.list.map((res) => {
 				return {
-					id: res.id,
+					karaoke_song_id: res.id,
+					origin: 'dailymotion',
 					link: `https://www.dailymotion.com/video${res.id}`,
-					thumbnail: {
-						small: res.thumbnail_120_url,
-						medium: res.thumbnail_360_url,
-						high: res.thumbnail_480_url,
-					},
-					published: undefined,
 					title: res.title,
-					language: res.language,
+					language: res.language || 'en',
 					duration: res.duration,
 					views: res.views_total,
 					likes: res.likes_total,
 					comments: 0,
-					origin: 'dailymotion',
+					thumbnail_small: res.thumbnail_120_url,
+					thumbnail_medium: res.thumbnail_360_url,
+					thumbnail_high: res.thumbnail_480_url,
 					similarity: stringSimilarity.compareTwoStrings(
 						res.title.toLowerCase(),
 						`karaoke ${search.toLowerCase()}`
